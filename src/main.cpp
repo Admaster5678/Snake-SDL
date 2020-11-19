08bdd9b3dc2ef5ae493eb9bb1ad0d3b9da38e838
Aditya Menon <adityaqatar10@gmail.com>
2020-11-19 11:52:46 +0300

    Set up the basic main function

--- src/main.cpp
+++ src/main.cpp
@@ -5,4 +5,9 @@
 #include <iostream>
 
 int main(int argc, char *argv[])
-{}
+{
+	if (SDL_Init(SDL_INIT_VIDEO) != 0)
+		std::cout << "SDL_Init Failed. Error Code: " << SDL_GetError() << std::endl;
+
+	return 0;
+}
